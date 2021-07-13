Ramsesdb/gexu-legacy-fork

Merge light and dark themes (#5470)
* Merge AMOLED and regular dark themes

This allows all variants of dark themes to use black backgrounds as a
separate preference.

* Merge light and dark themes

* Fix ReaderSeekBar color on Dark Blue theme

* Color fixes

* Fix Dark Blue bars ripple

* Simplify night mode check

app/src/main/java/eu/kanade/tachiyomi/data/preference/PreferenceValues.kt
@@ -1,5 +1,7 @@
 package eu.kanade.tachiyomi.data.preference
 
+import eu.kanade.tachiyomi.R
+
 const val UNMETERED_NETWORK = "wifi"
 const val CHARGING = "ac"
 
@@ -17,25 +19,17 @@ object PreferenceValues {
         system,
     }
 
-    // Keys are lowercase to match legacy string values
-    enum class LightThemeVariant {
-        default,
-        blue,
-        strawberrydaiquiri,
-        yotsuba
-    }
-
-    // Keys are lowercase to match legacy string values
-    enum class DarkThemeVariant {
-        default,
-        blue,
-        greenapple,
-        midnightdusk,
-        amoled,
-        hotpink,
-    }
-
     /* ktlint-enable experimental:enum-entry-name-case */
+
+    enum class AppTheme(val titleResId: Int) {
+        DEFAULT(R.string.theme_default),
+        DARK_BLUE(R.string.theme_darkblue),
+        GREEN_APPLE(R.string.theme_greenapple),
+        HOT_PINK(R.string.theme_hotpink),
+        MIDNIGHT_DUSK(R.string.theme_midnightdusk),
+        STRAWBERRY_DAIQUIRI(R.string.theme_strawberrydaiquiri),
+        YOTSUBA(R.string.theme_yotsuba)
+    }
 
     enum class TappingInvertMode(val shouldInvertHorizontal: Boolean = false, val shouldInvertVertical: Boolean = false) {
         NONE,

scb261/TachiyomiSY

(cherry picked from commit ad2bbd0e3c441491f5037e4644ba8bf33daf194e)

app/src/main/java/eu/kanade/tachiyomi/data/preference/PreferenceValues.kt
@@ -1,5 +1,7 @@
 package eu.kanade.tachiyomi.data.preference
 
+import eu.kanade.tachiyomi.R
+
 const val UNMETERED_NETWORK = "wifi"
 const val CHARGING = "ac"
 
@@ -17,27 +19,18 @@ object PreferenceValues {
         system,
     }
 
-    // Keys are lowercase to match legacy string values
-    enum class LightThemeVariant {
-        default,
-        blue,
-        strawberrydaiquiri,
-        yotsuba
-    }
-
-    // Keys are lowercase to match legacy string values
-    enum class DarkThemeVariant {
-        default,
-        blue,
-        greenapple,
-        midnightdusk,
-        amoled,
-        hotpink,
-        amoledblue,
-        red,
-    }
-
     /* ktlint-enable experimental:enum-entry-name-case */
+
+    enum class AppTheme(val titleResId: Int) {
+        DEFAULT(R.string.theme_default),
+        DARK_BLUE(R.string.theme_darkblue),
+        GREEN_APPLE(R.string.theme_greenapple),
+        HOT_PINK(R.string.theme_hotpink),
+        MIDNIGHT_DUSK(R.string.theme_midnightdusk),
+        STRAWBERRY_DAIQUIRI(R.string.theme_strawberrydaiquiri),
+        YOTSUBA(R.string.theme_yotsuba),
+        PURE_RED(R.string.theme_pure_red)
+    }
 
     enum class TappingInvertMode(val shouldInvertHorizontal: Boolean = false, val shouldInvertVertical: Boolean = false) {
         NONE,