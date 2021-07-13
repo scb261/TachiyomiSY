Ramsesdb/gexu-legacy-fork

Set root project name, remove jcenter for plugin resolution

settings.gradle.kts
@@ -13,8 +13,8 @@ pluginManagement {
         gradlePluginPortal()
         google()
         mavenCentral()
-        jcenter()
     }
 }
 
+rootProject.name = "Tachiyomi"
 include(":app")

scb261/TachiyomiSY

(cherry picked from commit 5801297d78e7c04e85e0e805b81bbcfc6633e4d8)
(cherry picked from commit c07cc7f0df3cadc2d08f6831525472a6ec09fc65)

settings.gradle.kts
@@ -13,8 +13,8 @@ pluginManagement {
         gradlePluginPortal()
         google()
         mavenCentral()
-        jcenter()
     }
 }
 
+rootProject.name = "TachiyomiXZM"
 include(":app")