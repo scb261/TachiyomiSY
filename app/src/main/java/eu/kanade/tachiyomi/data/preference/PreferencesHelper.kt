Ramsesdb/gexu

Update to Conductor 3.0.0
(cherry picked from commit 01a1a9ebab09bb81fcb35412b171be794bfb91bf)

# Conflicts:
#	app/src/main/java/eu/kanade/tachiyomi/ui/browse/migration/search/SourceSearchController.kt

app/src/main/java/eu/kanade/tachiyomi/data/preference/PreferencesHelper.kt
@@ -9,7 +9,7 @@ import com.tfcporciuncula.flow.FlowSharedPreferences
 import com.tfcporciuncula.flow.Preference
 import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.data.database.models.Manga
-import eu.kanade.tachiyomi.data.preference.PreferenceValues.ThemeMode.*
+import eu.kanade.tachiyomi.data.preference.PreferenceValues.ThemeMode.system
 import eu.kanade.tachiyomi.data.track.TrackService
 import eu.kanade.tachiyomi.data.track.anilist.Anilist
 import eu.kanade.tachiyomi.ui.library.setting.DisplayModeSetting

scb261/TachiyomiSY

(cherry picked from commit b71694fb542352420f6e8945a7f1f52d5a07e4a2)

app/src/main/java/eu/kanade/tachiyomi/data/preference/PreferencesHelper.kt
@@ -9,7 +9,7 @@ import com.tfcporciuncula.flow.FlowSharedPreferences
 import com.tfcporciuncula.flow.Preference
 import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.data.database.models.Manga
-import eu.kanade.tachiyomi.data.preference.PreferenceValues.ThemeMode.*
+import eu.kanade.tachiyomi.data.preference.PreferenceValues.ThemeMode.dark
 import eu.kanade.tachiyomi.data.track.TrackService
 import eu.kanade.tachiyomi.data.track.anilist.Anilist
 import eu.kanade.tachiyomi.ui.library.setting.DisplayModeSetting