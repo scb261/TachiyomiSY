691efa00438f27ece8e2040ef46051b9ad93f3e7
Jobobby04 <jobobby04@users.noreply.github.com>
2021-09-01 18:37:53 -0400

    Temp fix for clear db, dont click it
    (cherry picked from commit 19a00ce582a1615ce8ac1a747879ee37de5a0672)

--- app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsAdvancedController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsAdvancedController.kt
@@ -7,10 +7,14 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.net.toUri
 import androidx.core.text.HtmlCompat
+import androidx.core.view.setMargins
 import androidx.preference.PreferenceScreen
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import eu.kanade.tachiyomi.R
@@ -43,6 +47,7 @@ import eu.kanade.tachiyomi.util.preference.summaryRes
 import eu.kanade.tachiyomi.util.preference.switchPreference
 import eu.kanade.tachiyomi.util.preference.titleRes
 import eu.kanade.tachiyomi.util.storage.DiskUtil
+import eu.kanade.tachiyomi.util.system.dpToPx
 import eu.kanade.tachiyomi.util.system.powerManager
 import eu.kanade.tachiyomi.util.system.toast
 import exh.debug.SettingsDebugController
@@ -432,7 +437,7 @@ class SettingsAdvancedController : SettingsController() {
             )
             val selected = booleanArrayOf(true)
             return MaterialAlertDialogBuilder(activity!!)
-                .setMessage(R.string.clear_database_confirmation)
+                // .setMessage(R.string.clear_database_confirmation)
                 // SY -->
                 .setMultiChoiceItems(item, selected) { _, _, isChecked ->
                     selected[0] = isChecked
@@ -442,7 +447,27 @@ class SettingsAdvancedController : SettingsController() {
                     (targetController as? SettingsAdvancedController)?.clearDatabase(selected.first())
                 }
                 .setNegativeButton(android.R.string.cancel, null)
-                .create()
+                .create().apply {
+                    LinearLayout(this.context).apply {
+                        layoutParams = LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.MATCH_PARENT,
+                            LinearLayout.LayoutParams.WRAP_CONTENT
+                        )
+                        TextView(this.context).apply {
+                            layoutParams = LinearLayout.LayoutParams(
+                                ViewGroup.LayoutParams.MATCH_PARENT,
+                                ViewGroup.LayoutParams.WRAP_CONTENT
+                            ).apply {
+                                setMargins(8.dpToPx)
+                            }
+                            setText(R.string.clear_database_confirmation)
+                            isClickable = false
+                            isFocusable = false
+                        }.let(this::addView)
+                        isClickable = false
+                        isFocusable = false
+                    }.let(listView::addFooterView)
+                }
         }
     }
 
