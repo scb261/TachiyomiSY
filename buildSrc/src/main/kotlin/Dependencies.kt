02d5c4aad34f29429e0c7c16f77ea27c43f26f89
arkon <eugcheung94@gmail.com>
2021-09-04 17:09:33 +0300

    Update dependencies
    (cherry picked from commit 2d3a1b6a9e8c5ccf84c6ae6e94b46be96a486a4b)
    
    (cherry picked from commit 74c394193a0c94f639c815f394281ff1d8b77846)

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,7 +1,7 @@
 object BuildPluginsVersion {
     const val AGP = "4.2.2"
     const val KOTLIN = "1.5.30"
-    const val KOTLINTER = "3.5.1"
+    const val KOTLINTER = "3.6.0"
     const val VERSIONS_PLUGIN = "0.39.0"
     const val ABOUTLIB_PLUGIN = "8.9.1"
 }
