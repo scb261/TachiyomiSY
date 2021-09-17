d995f3af37efbe1507981fd290019174526300fd
Jobobby04 <jobobby04@users.noreply.github.com>
2021-09-17 15:40:39 -0400

    Fix more info crash
    (cherry picked from commit 7ffe1794d9135098278fce8bc2012fa17cb5b9fe)

--- app/src/main/java/eu/kanade/tachiyomi/ui/manga/MangaController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/manga/MangaController.kt
@@ -486,7 +486,6 @@ class MangaController :
     override fun onDestroyView(view: View) {
         destroyActionModeIfNeeded()
         binding.actionToolbar.destroy()
-        mangaInfoAdapter?.onDestroyView()
         mangaInfoAdapter = null
         chaptersHeaderAdapter = null
         chaptersAdapter = null
