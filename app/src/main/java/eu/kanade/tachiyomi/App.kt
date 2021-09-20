33fc9fc79756c275d72df4dadff27d868a321a7b
arkon <eugcheung94@gmail.com>
2021-09-20 21:33:35 +0300

    Update dependencies
    (cherry picked from commit 9de08c8166749a8959f27c78185060b00269e372)
    
    # Conflicts:
    #	app/src/main/java/eu/kanade/tachiyomi/App.kt
    
    (cherry picked from commit b2ee3bf176c8b4ff421f0e1391f95b912da0e321)

--- app/src/main/java/eu/kanade/tachiyomi/App.kt
+++ app/src/main/java/eu/kanade/tachiyomi/App.kt
@@ -14,9 +14,8 @@ import android.webkit.WebView
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.getSystemService
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleObserver
-import androidx.lifecycle.OnLifecycleEvent
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ProcessLifecycleOwner
 import androidx.lifecycle.lifecycleScope
 import coil.ImageLoader
@@ -69,14 +68,14 @@ import java.security.Security
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-open class App : Application(), LifecycleObserver, ImageLoaderFactory {
+open class App : Application(), DefaultLifecycleObserver, ImageLoaderFactory {
 
     private val preferences: PreferencesHelper by injectLazy()
 
     private val disableIncognitoReceiver = DisableIncognitoReceiver()
 
     override fun onCreate() {
-        super.onCreate()
+        super<Application>.onCreate()
         // if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
         setupExhLogging() // EXH logging
         Timber.plant(XLogTree()) // SY Redirect Timber to XLog
@@ -167,9 +166,7 @@ open class App : Application(), LifecycleObserver, ImageLoaderFactory {
         }
     }
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
-    @Suppress("unused")
-    fun onAppBackgrounded() {
+    override fun onStop(owner: LifecycleOwner) {
         if (preferences.lockAppAfter().get() >= 0) {
             SecureActivityDelegate.locked = true
         }
