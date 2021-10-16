bad4e100c9c1ab7ff2ff3665e0906ee0a7909f6c
arkon <eugcheung94@gmail.com>
2021-10-16 10:42:14 -0400

    Add notification action to open GitHub release page
    Co-authored-by: Jays2Kings <Jays2Kings@users.noreply.github.com>
    (cherry picked from commit 3d8f123e053c4612cda5feebca3c18f93fff7ae4)
    (cherry picked from commit 01778c0c4273eac6a10b492e64f8f0cc5deec624)

--- app/src/main/java/eu/kanade/tachiyomi/data/updater/AppUpdateChecker.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/updater/AppUpdateChecker.kt
@@ -1,5 +1,6 @@
 package eu.kanade.tachiyomi.data.updater
 
+import android.content.Context
 import eu.kanade.tachiyomi.BuildConfig
 import eu.kanade.tachiyomi.data.preference.PreferencesHelper
 import eu.kanade.tachiyomi.network.GET
@@ -22,9 +23,9 @@ class AppUpdateChecker {
         // SY <--
     }
 
-    suspend fun checkForUpdate(): AppUpdateResult {
+    suspend fun checkForUpdate(context: Context): AppUpdateResult {
         return withIOContext {
-            networkService.client
+            val result = networkService.client
                 .newCall(GET("https://api.github.com/repos/$repo/releases/latest"))
                 .await()
                 .parseAs<GithubRelease>()
@@ -38,6 +39,12 @@ class AppUpdateChecker {
                         AppUpdateResult.NoNewUpdate
                     }
                 }
+
+            if (result is AppUpdateResult.NewUpdate) {
+                AppUpdateNotifier(context).promptUpdate(result.release)
+            }
+
+            result
         }
     }
 
