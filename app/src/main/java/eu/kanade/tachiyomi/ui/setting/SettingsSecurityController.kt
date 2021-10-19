5ff07541c13393497a82cd4cfc916ced1cb42bd9
Jobobby04 <jobobby04@users.noreply.github.com>
2021-10-18 23:09:26 -0400

    Cleanup and fixes
    (cherry picked from commit 2f203f7a1d068f837f2771d774c89e8d06458f85)

--- app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsSecurityController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/SettingsSecurityController.kt
@@ -71,6 +71,7 @@ class SettingsSecurityController : SettingsController() {
             titleRes = R.string.hide_notification_content
             defaultValue = false
         }
+        // SY -->
         preference {
             key = "pref_edit_lock_times"
             titleRes = R.string.action_edit_biometric_lock_times
@@ -97,8 +98,10 @@ class SettingsSecurityController : SettingsController() {
                 SetLockedDaysDialog().showDialog(router)
             }
         }
+        // SY <--
     }
 
+    // SY -->
     class SetLockedDaysDialog(bundle: Bundle? = null) : DialogController(bundle) {
         val preferences: PreferencesHelper by injectLazy()
 
@@ -160,4 +163,5 @@ class SettingsSecurityController : SettingsController() {
                 .create()
         }
     }
+    // SY <--
 }
