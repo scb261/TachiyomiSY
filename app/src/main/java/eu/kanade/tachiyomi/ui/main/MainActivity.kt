0ab7d18ad31f764134c34773d52e9745edf8fd95
sdaqo <sdaqo.dev@protonmail.com>
2025-01-12 04:38:05 +0100

    Add option to enable incognito mode per extension (#157)
    * add per Extension Incognito Mode
    
    * migrate incognito sources when extension is updated
    
    * remove incognito sources when extension is uninstalled
    
    * remove not used variable
    
    * address change requests
    
    address change requests
    
    * Rebase and cleanup code
    
    ---------
    
    Co-authored-by: AntsyLich <59261191+AntsyLich@users.noreply.github.com>
    (cherry picked from commit c283abefb03f79ce6652492db71cde410f828f78)
    
    # Conflicts:
    #	CHANGELOG.md
    #	app/src/main/java/eu/kanade/tachiyomi/extension/ExtensionManager.kt
    #	app/src/main/java/eu/kanade/tachiyomi/ui/browse/source/browse/BrowseSourceScreenModel.kt
    #	app/src/main/java/eu/kanade/tachiyomi/ui/reader/ReaderViewModel.kt

--- app/src/main/java/eu/kanade/tachiyomi/ui/main/MainActivity.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/main/MainActivity.kt
@@ -54,6 +54,7 @@ import cafe.adriel.voyager.navigator.currentOrThrow
 import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.ktx.Firebase
 import eu.kanade.domain.base.BasePreferences
+import eu.kanade.domain.source.interactor.GetIncognitoState
 import eu.kanade.presentation.components.AppStateBanners
 import eu.kanade.presentation.components.DownloadedOnlyBannerBackgroundColor
 import eu.kanade.presentation.components.IncognitoModeBannerBackgroundColor
@@ -122,6 +123,8 @@ class MainActivity : BaseActivity() {
     private val downloadCache: DownloadCache by injectLazy()
     private val chapterCache: ChapterCache by injectLazy()
 
+    private val getIncognitoState: GetIncognitoState by injectLazy()
+
     // To be checked by splash screen. If true then splash screen will be removed.
     var ready = false
 
@@ -181,7 +184,7 @@ class MainActivity : BaseActivity() {
         setComposeContent {
             val context = LocalContext.current
 
-            val incognito by preferences.incognitoMode().collectAsState()
+            var incognito by remember { mutableStateOf(getIncognitoState.await(null)) }
             val downloadOnly by preferences.downloadedOnly().collectAsState()
             val indexing by downloadCache.isInitializing.collectAsState()
 
@@ -230,6 +233,11 @@ class MainActivity : BaseActivity() {
                         }
                         // SY <--
                     }
+                }
+                LaunchedEffect(navigator.lastItem) {
+                    (navigator.lastItem as? BrowseSourceScreen)?.sourceId
+                        .let(getIncognitoState::subscribe)
+                        .collectLatest { incognito = it }
                 }
 
                 val scaffoldInsets = WindowInsets.navigationBars.only(WindowInsetsSides.Horizontal)
